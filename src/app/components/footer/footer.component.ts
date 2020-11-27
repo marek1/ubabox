6b351c2ceb05f3ff5ecb6110a8d9aa4ba692ecc5
marek1 <cyclingfellas@gmail.com>
2020-11-27 10:08:41 +0100

    1.0.1 : Produkte link

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ROUTE_PRODUCTS } from '../../routes';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  public ROUTE_PRODUCTS = ROUTE_PRODUCTS;
   constructor() { }
 
   ngOnInit() {
